13255dec05dc884f45b9c025d1c6e04925a80713
Marcus Sarnes <70203239+mackans95@users.noreply.github.com>
2021-05-26 14:44:27 +0200

    initial fixes to run

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 //IMPORTS
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 // const bodyParser = require('body-parser');
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 //import Routes
-const mainRoutes = require('./routes/mainRoutes');
-const aimGaimRoutes = require('./routes/aimGaimRoutes');
+const mainRoutes = require("./routes/mainRoutes");
+const aimGaimRoutes = require("./routes/aimGaimRoutes");
 //END IMPORTS
 
 app.use(express.static(publicDirectoryPath));
@@ -17,17 +17,18 @@ app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 //END Middleware
 
-app.use('/', mainRoutes);
-app.use('/', aimGaimRoutes);
+app.use("/", mainRoutes);
+app.use("/", aimGaimRoutes);
 
 //Conntect to DB
-//'mongodb+srv://gitgudadmin:skollosenord@cluster0.k5q5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(process.env.DB_CONNECTION, 
-    { useUnifiedTopology: true }, 
-    ()=>{
-        console.log('connected to db!');
-    });
+const connection =
+  "mongodb+srv://gitgudadmin:skollosenord@cluster0.k5q5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// process.env.DB_CONNECTION
+
+mongoose.connect(connection, { useUnifiedTopology: true }, () => {
+  console.log("connected to db!");
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+  console.log("Server is up on port 3000.");
 });
