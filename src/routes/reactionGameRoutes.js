81939b8ffe556e3716734a4e13fa542c574a8175
Calle Persson <Calle_1997@hotmail.se>
2021-05-30 12:02:42 +0200

    created auth. highscores saves with logged in user as username

--- src/routes/reactionGameRoutes.js
+++ src/routes/reactionGameRoutes.js
@@ -3,19 +3,21 @@ const router = new express.Router();
 const path = require('path');
 const Highscore = require('../models/highscores');
 const user = require('../models/users');
+const auth = require('../controllers/auth')
 
 const publicDirectoryPath = path.join(__dirname, '../../public');
 router.use(express.static(publicDirectoryPath));
 
-router.get('/ReactionGame', (req, res) => {
+router.get('/ReactionGame', auth, (req, res) => {
     res.sendFile(publicDirectoryPath +  '/reaction.html');
-    console.log("reactionGameRoute");
+    // console.log("reactionGameRoute");
 })
 
-router.post('/ReactionGame', async (req,res) => {
+router.post('/ReactionGame', auth, async (req,res) => {
 
-    const highscore = await Highscore(req.body);
     try{
+        // hämtar all data från auth. Utom score
+        const highscore = await Highscore({score: req.body.score, username: req.user.username, gamename: req.game});
         await highscore.save();
         res.status(201).send(highscore);
 
