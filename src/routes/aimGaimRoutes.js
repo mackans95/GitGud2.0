60f7e1bed4049bdbe77f7642794d4e64f8238ab0
C_Boman <ohdaesu@hotmail.se>
2021-06-01 12:57:43 +0200

    Add display top 5 personal and global leaderboards

--- src/routes/aimGaimRoutes.js
+++ src/routes/aimGaimRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
 
 const publicDirectoryPath = path.join(__dirname, '../../public');
 router.use(express.static(publicDirectoryPath));
@@ -10,10 +11,37 @@ const User = require('../models/users');
 const authTokenMiddleware = require("../controllers/userAuth");
 
 //ROUTES /AimGaim
-router.get('/AimGaim', authTokenMiddleware, (req, res) => {
+router.get('/AimGaim', authTokenMiddleware, async (req, res) => {
+
     res.sendFile(publicDirectoryPath +  '/AimGaim.html');
+
     console.log("AimGameRoute");
 })
+
+router.get('/AimGaim/personalHighscores', authTokenMiddleware, async (req, res) => {
+    //send leaderboards
+    const user = await User.findById(req.user.id);
+    const personalLeaderboard = await Highscore.find({"username" : user.username, "gamename" : "AimGaim"});
+    personalLeaderboard.sort((a, b) => {return b.score - a.score});
+
+    var arrayToString = JSON.stringify(Object.assign({}, personalLeaderboard));  // convert array to string
+    var stringToJsonObject = JSON.parse(arrayToString);  // convert string to json object
+
+    res.json(stringToJsonObject);
+})
+router.get('/AimGaim/globalHighscores', authTokenMiddleware, async (req, res) => {
+  //send leaderboards
+  const globalLeaderboard = await Highscore.find({"gamename" : "AimGaim"});
+  globalLeaderboard.sort((a, b) => {return b.score - a.score});
+  const slicedglobalLeaderboard = globalLeaderboard.slice(0, 5);
+
+  // console.log(slicedglobalLeaderboard);
+  var arrayToString = JSON.stringify(Object.assign({}, slicedglobalLeaderboard));  // convert array to string
+  var stringToJsonObject = JSON.parse(arrayToString);  // convert string to json object
+
+  res.json(stringToJsonObject);
+})
+
 
 //post routes
 router.post('/AimGaim', authTokenMiddleware, async (req, res)=>{
@@ -44,8 +72,32 @@ router.post('/AimGaim', authTokenMiddleware, async (req, res)=>{
       return res.status(400).send('No logged in user');
     }
     const userName = user.username;
-    // console.log(req.body);
+
     req.body.username = userName;
+
+    //compare to other highscores
+    let newHsHigher = false;
+    const playersHighscores = await Highscore.find({"username" : userName, "gamename" : "AimGaim"});
+
+    //if there are 5 scores saved for the user already, check if this one is better
+    if(playersHighscores.length > 4){  
+      playersHighscores.forEach(hs => {
+        if(hs.score < req.body.score){
+          newHsHigher = true;
+        }
+      });
+
+      if(!newHsHigher){
+        return res.status(200).send("Score isn't good enough");
+      }
+      else{
+        //delete the lowest score, by first ordering them
+        playersHighscores.sort((a, b)=>{
+          return a.score - b.score;
+        })
+        await Highscore.remove({ _id : ObjectId(playersHighscores[0]._id) });
+      }
+    }
 
     const highscore2 = await Highscore.create(req.body);
     res.status(201).json({
@@ -57,3 +109,4 @@ router.post('/AimGaim', authTokenMiddleware, async (req, res)=>{
 })
 
 module.exports = router;
+
